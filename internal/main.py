4adbbbc676c8a515f91626153094ea76532c0367
Denis Balashov <balasovdenis508@gmail.com>
2025-03-11 18:41:29 +0300

    добавил землю

--- internal/main.py
+++ internal/main.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 from player import Hero
+from ground import Ground
 from  GameSprite import GSprite
 screen = pygame.display.set_mode((WIGTH,HEIGHT),pygame.RESIZABLE)
 bg = pygame.image.load('assets/background/fon.jpg')
@@ -10,9 +11,12 @@ game = True
 FPS = 60
 clock = pygame.time.Clock()
 hero = Hero('assets/error.png', WIGTH//2,(HEIGHT//7)*4,3,WIGTH//16,HEIGHT//5)
+ground = Ground('assets/floor/floor.png', 0,(HEIGHT//7)*6,3,WIGTH,HEIGHT//5)
 while True:
     screen.blit(bg, (0, 0))
     hero.reset(screen)
+    ground.reset(screen)
+    ground.collide(hero)
     hero.walk()
     hero.walk_anim_right()
     hero.walk_anim_left()
